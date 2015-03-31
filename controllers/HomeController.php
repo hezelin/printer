6030e01e1e97e7cff31a4089c2d4ee4eb8aa9948
Harry <730107711@qq.com>
2015-03-31 11:29:37 +0800

    生成app控制器+home.less + 维修官网home/maintain

--- controllers/HomeController.php
+++ controllers/HomeController.php
@@ -6,21 +6,41 @@ class HomeController extends \yii\web\Controller
 {
     public $layout = 'console';
 
+    /*
+     * 店铺装修后台
+     */
     public function actionFitment()
     {
         return $this->render('fitment');
     }
 
+    /*
+     * 微官网
+     */
     public function actionIndex()
     {
         return $this->render('index');
     }
 
+    /*
+     * 维修员官网
+     */
+    public function actionMaintain()
+    {
+        return $this->render('maintain');
+    }
+
+    /*
+     * 官网设置
+     */
     public function actionSetting()
     {
         return $this->render('setting');
     }
 
+    /*
+     * 样式选择
+     */
     public function actionStyle()
     {
         return $this->render('style');
