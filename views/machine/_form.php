d51427466dfabe6df5807b5c5959624f21d3d4f1
Harry <730107711@qq.com>
2015-05-04 18:19:01 +0800

    更多属性值，弹出选择

--- views/machine/_form.php
+++ views/machine/_form.php
@@ -4,6 +4,8 @@ use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 use dosamigos\datepicker\DatePicker;
 use karpoff\icrop\CropImageUpload;
+use yii\bootstrap\Modal;
+
 
 /* @var $this yii\web\View */
 /* @var $model app\models\TblMachine */
@@ -29,7 +31,6 @@ use karpoff\icrop\CropImageUpload;
     <?= $form->field($model, 'price')->textInput() ?>
     <?= $form->field($model, 'monthly_rent')->textInput() ?>
     <?= $form->field($model, 'function')->textInput(['placeholder'=>'例如：双面复印，网络打印']) ?>
-
     <?= $form->field($model, 'buy_time')->widget(\yii\jui\DatePicker::classname(), [
         'dateFormat' => 'yyyy-MM-dd',
         'options' => [
@@ -41,6 +42,10 @@ use karpoff\icrop\CropImageUpload;
         if( $model->isNewRecord )
            echo  $form->field($model, 'amount')->textInput(['value'=>1]);
     ?>
+
+    <?= $form->field($model, 'else_attr')->textInput(['placeholder'=>'属性名，属性值一一对应']) ?>
+
+
     <div class="form-group">
         <div class="col-lg-offset-1 col-lg-11">
             <div class="m-l-12">
@@ -52,3 +57,74 @@ use karpoff\icrop\CropImageUpload;
     <?php ActiveForm::end(); ?>
 
 </div>
+
+<?php
+Modal::begin([
+    'header' => '补充属性',
+    'id' => 'Moreattr-modal',
+    'size' => 'modal-md',
+    'toggleButton' => false,
+    'footer' => '
+        <button type="button" class="btn btn-default" data-dismiss="modal">关闭</button>
+        <button type="button" class="btn btn-default add-row">增加一列</button>
+        <button id="Moreattr-save" type="button" class="btn btn-primary">保存属性</button>
+    ',
+]);
+
+echo Html::beginTag('form',['class'=>'form-inline','id'=>'my-city']);
+
+
+echo '
+<div class="form-group m-b-5">
+<input type="text" class="form-control" name="attr[]" value="" placeholder="属性名">
+&nbsp;&nbsp;
+<input type="text" class="form-control" name="attr[]" value="" placeholder="属性值">&nbsp;&nbsp;
+<button type="button" class="btn btn-danger del-row"><i class="glyphicon glyphicon-trash"></i> </button>
+</div>
+';
+
+
+echo Html::endTag('form');
+
+Modal::end();
+?>
+
+<script>
+    <?php $this->beginBlock('JS_END') ?>
+    var tpl = '<div class="form-group m-b-5"><input type="text" class="form-control" name="attr[]" value="" placeholder="属性名"> &nbsp;&nbsp; <input type="text" class="form-control" name="attr[]" value="" placeholder="属性值">&nbsp;&nbsp; <button type="button" class="btn btn-danger del-row"><i class="glyphicon glyphicon-trash"></i> </button> </div>';
+
+    $('#Moreattr-modal .add-row').click(function(){
+        $('#my-city').append(tpl);
+    });
+    $('#Moreattr-modal').on('click','.del-row',function(){
+        $(this).closest('.form-group').remove();
+    });
+
+    $('#Moreattr-save').click(function(){
+        var tmp = [];
+        var i=0;
+        var data = {};
+        $("#Moreattr-modal input[name='attr[]']").each(function(){
+            if( i%2 == 0 ){
+                data.name = $(this).val();
+            }
+            else{
+                data.value= $(this).val();
+                tmp.push(data );
+            }
+            i++;
+        });
+        $('#tblmachine-else_attr').val(JSON.stringify(tmp) );
+        $('#Moreattr-modal').modal('hide');
+    });
+
+    $('#tblmachine-else_attr').click(function(){
+        $('#Moreattr-modal').modal('show');
+    });
+
+    <?php $this->endBlock();?>
+</script>
+
+<?php
+    $this->registerJs($this->blocks['JS_END'],\yii\web\View::POS_READY);
+?>
