b8a8597cdffad5d18aa0f7250a8664cd22506a0b
Harry <730107711@qq.com>
2015-05-05 18:03:05 +0800

    用户首页 url 链接增加关联的微信id

--- views/home/index.php
+++ views/home/index.php
@@ -12,10 +12,10 @@ $this->registerCssFile('/css/home/'.$setting['style']);
 <?= CarouselWidget::widget(['data'=>$setting['carousel']]) ?>
 <!--首页菜单-->
 <ul id="main-menu">
-    <li><a href="<?= Url::toRoute(['i/machine']) ?>" ><b><img src="/images/tb01.png" /></b><span>我的机器</span></a></li>
-    <li><a href="<?= Url::toRoute(['share/score']) ?>" ><b><img src="/images/tb07.png" /></b><span>赚取积分</span></a></li>
-    <li><a href="<?= Url::toRoute(['i/service']) ?>" ><b><img src="/images/tb03.png" /></b><span>维修服务</span></a></li>
-    <li><a href="<?= Url::toRoute(['share/active']) ?>" ><b><img src="/images/tb08.png" /></b><span>最新活动</span></a></li>
-    <li><a href="<?= Url::toRoute(['mall/index']) ?>" ><b><img src="/images/tb05.png" /></b><span>微商城</span></a></li>
-    <li><a href="<?= Url::toRoute(['share/game']) ?>" ><b><img src="/images/tb06.png" /></b><span>游戏中心</span></a></li>
+    <li><a href="<?= Url::toRoute(['i/machine','id'=>$setting['wx_id']]) ?>" ><b><img src="/images/tb01.png" /></b><span>我的机器</span></a></li>
+    <li><a href="<?= Url::toRoute(['share/score','id'=>$setting['wx_id']]) ?>" ><b><img src="/images/tb07.png" /></b><span>赚取积分</span></a></li>
+    <li><a href="<?= Url::toRoute(['i/service','id'=>$setting['wx_id']]) ?>" ><b><img src="/images/tb03.png" /></b><span>维修服务</span></a></li>
+    <li><a href="<?= Url::toRoute(['share/active','id'=>$setting['wx_id']]) ?>" ><b><img src="/images/tb08.png" /></b><span>最新活动</span></a></li>
+    <li><a href="<?= Url::toRoute(['mall/index','id'=>$setting['wx_id']]) ?>" ><b><img src="/images/tb05.png" /></b><span>微商城</span></a></li>
+    <li><a href="<?= Url::toRoute(['share/game','id'=>$setting['wx_id']]) ?>" ><b><img src="/images/tb06.png" /></b><span>游戏中心</span></a></li>
 </ul>
