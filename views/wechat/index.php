0cb26ae84c12f165fb05c69d235eae82816a88a4
Harry <730107711@qq.com>
2015-05-08 18:15:01 +0800

    机器扫描跳转

--- views/wechat/index.php
+++ views/wechat/index.php
@@ -14,8 +14,25 @@ $this->registerCssFile('/css/home/'.$setting['style']);
 <ul id="main-menu">
     <li><a href="<?= Url::toRoute(['i/machine','id'=>$setting['wx_id']]) ?>" ><b><img src="/images/tb01.png" /></b><span>我的机器</span></a></li>
     <li><a href="<?= Url::toRoute(['share/score','id'=>$setting['wx_id']]) ?>" ><b><img src="/images/tb07.png" /></b><span>赚取积分</span></a></li>
-    <li><a href="<?= Url::toRoute(['i/service','id'=>$setting['wx_id']]) ?>" ><b><img src="/images/tb03.png" /></b><span>维修服务</span></a></li>
-    <li><a href="<?= Url::toRoute(['share/active','id'=>$setting['wx_id']]) ?>" ><b><img src="/images/tb08.png" /></b><span>最新活动</span></a></li>
+    <li><a id="scan-btn" href="javascript:void(0);" ><b><img src="/images/tb03.png" /></b><span>扫描</span></a></li>
     <li><a href="<?= Url::toRoute(['mall/index','id'=>$setting['wx_id']]) ?>" ><b><img src="/images/tb05.png" /></b><span>微商城</span></a></li>
+    <li><a href="<?= Url::toRoute(['share/active','id'=>$setting['wx_id']]) ?>" ><b><img src="/images/tb08.png" /></b><span>最新活动</span></a></li>
     <li><a href="<?= Url::toRoute(['share/game','id'=>$setting['wx_id']]) ?>" ><b><img src="/images/tb06.png" /></b><span>游戏中心</span></a></li>
 </ul>
+
+<?php
+\app\components\WxjsapiWidget::widget([
+    'wx_id'=>$setting['wx_id'],
+    'apiList'=>['scanQRCode'],
+    'jsReady'=>'
+    document.querySelector("#scan-btn").onclick = function () {
+        wx.scanQRCode({
+            needResult: 0, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
+            scanType: ["qrCode","barCode"], // 可以指定扫二维码还是一维码，默认二者都有
+            success: function (res) {
+            var result = res.resultStr; // 当needResult 为 1 时，扫码返回的结果
+        }
+        });
+    };'
+])
+?>
