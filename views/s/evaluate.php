958a22fcfa3855324a7dfc991536bc49788025f9
Harry <730107711@qq.com>
2015-05-23 13:59:37 +0800

    用户维修评价页面更改

--- views/s/evaluate.php
+++ views/s/evaluate.php
@@ -4,6 +4,7 @@ use yii\helpers\Html;
 ?>
 <style>
     body{ background-color: #ffffff !important;}
+    .h-row{ margin-bottom: 2%; margin-top:0; float: left;}
 </style>
 
 <?php
@@ -11,27 +12,40 @@ if( Yii::$app->session->hasFlash('error') )
     echo Html::tag('div',Yii::$app->session->getFlash('error'),['class'=>'h-error']);
 ?>
 <div class="h-center-wrap">
-    <div class="h-form">
         <form method="post" id="wechat-form">
             <input name="_csrf" type="hidden" value="<?=\Yii::$app->request->csrfToken?>"/>
-            <input type="hidden" id="service-imgid" class="h-input" name="TblMachineService[imgid]" />
             <input type="hidden"  name="TblServiceEvaluate[fault_id]" value="<?=$id?>" />
-            <p>&nbsp;</p>
             <div class="h-row">
-                <div class="h-label">态度</div>
-                <?= Html::dropDownList('TblServiceEvaluate[attitude]','',['糟糕','一般','满意'],['class'=>'h-label-input'])?>
+                <div class="h-label">
+                    <input type="radio" id="score1" name="TblServiceEvaluate[score]" value="5" checked/>
+                </div>
+                <label class="h-label" for="score1">很满意</label>
             </div>
             <div class="h-row">
-                <div class="h-label">速度</div>
-                <?= Html::dropDownList('TblServiceEvaluate[speed]','',['比较慢','一般','比较快'],['class'=>'h-label-input'])?>
+                <div class="h-label">
+                    <input type="radio" id="score2" name="TblServiceEvaluate[score]" value="4"  />
+                </div>
+                <label class="h-label" for="score2">满意</label>
             </div>
             <div class="h-row">
-                <div class="h-label">质量</div>
-                <?= Html::dropDownList('TblServiceEvaluate[quality]','',['没修好','正常','非常好'],['class'=>'h-label-input'])?>
+                <div class="h-label">
+                    <input type="radio" id="score3" name="TblServiceEvaluate[score]" value="3"  />
+                </div>
+                <label class="h-label" for="score3">一般</label>
             </div>
-            <br/>
-            <br/>
+            <div class="h-row">
+                <div class="h-label">
+                    <input type="radio" id="score4" name="TblServiceEvaluate[score]" value="2"  />
+                </div>
+                <label class="h-label" for="score4">不满意</label>
+            </div>
+            <div class="h-row">
+                <div class="h-label">
+                    <input type="radio" id="score5" name="TblServiceEvaluate[score]" value="1"  />
+                </div>
+                <label class="h-label" for="score5">很不满意</label>
+            </div>
+            <div class="h-row">&nbsp;</div>
             <button type="submit" id="wechat-submit" class="h-button">提交评价</button>
         </form>
-    </div>
 </div>
