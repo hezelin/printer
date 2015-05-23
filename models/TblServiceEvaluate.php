bab82150c754f3799d6a8e1b464c999408d7222e
Harry <730107711@qq.com>
2015-05-23 14:00:53 +0800

    机器维修评价 model

--- models/TblServiceEvaluate.php
+++ models/TblServiceEvaluate.php
@@ -4,49 +4,28 @@ namespace app\models;
 
 use Yii;
 
-/**
- * This is the model class for table "tbl_service_evaluate".
- *
- * @property string $id
- * @property string $fault_id
- * @property integer $quality
- * @property integer $speed
- * @property integer $attitude
- * @property string $add_time
- */
 class TblServiceEvaluate extends \yii\db\ActiveRecord
 {
-    /**
-     * @inheritdoc
-     */
     public static function tableName()
     {
         return 'tbl_service_evaluate';
     }
 
-    /**
-     * @inheritdoc
-     */
     public function rules()
     {
         return [
-            [['fault_id', 'quality', 'speed', 'attitude', 'add_time'], 'required'],
-            [['fault_id', 'quality', 'speed', 'attitude', 'add_time'], 'integer']
+            [['fault_id', 'add_time'], 'required'],
+            [['fault_id', 'score', 'add_time'], 'integer']
         ];
     }
 
-    /**
-     * @inheritdoc
-     */
     public function attributeLabels()
     {
         return [
-            'id' => 'ID',
-            'fault_id' => 'Fault ID',
-            'quality' => 'Quality',
-            'speed' => 'Speed',
-            'attitude' => 'Attitude',
-            'add_time' => 'Add Time',
+            'id' => '自增id',
+            'fault_id' => '维修id',
+            'score' => '评分',
+            'add_time' => '评价时间',
         ];
     }
 }
