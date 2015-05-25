299166e52c42ec5aedb1ab6eb2bd636dcd0bb7b2
Harry <730107711@qq.com>
2015-05-25 15:31:02 +0800

    故障进度字段长度修改，支持多张图片

--- models/TblServiceProcess.php
+++ models/TblServiceProcess.php
@@ -4,48 +4,30 @@ namespace app\models;
 
 use Yii;
 
-/**
- * This is the model class for table "tbl_service_process".
- *
- * @property string $id
- * @property string $service_id
- * @property string $content
- * @property integer $process
- * @property string $add_time
- */
 class TblServiceProcess extends \yii\db\ActiveRecord
 {
-    /**
-     * @inheritdoc
-     */
     public static function tableName()
     {
         return 'tbl_service_process';
     }
 
-    /**
-     * @inheritdoc
-     */
     public function rules()
     {
         return [
             [['service_id', 'content', 'add_time'], 'required'],
             [['service_id', 'process', 'add_time'], 'integer'],
-            [['content'], 'string', 'max' => 500]
+            [['content'], 'string', 'max' => 1000]
         ];
     }
 
-    /**
-     * @inheritdoc
-     */
     public function attributeLabels()
     {
         return [
-            'id' => 'ID',
-            'service_id' => 'Service ID',
-            'content' => 'Content',
-            'process' => 'Process',
-            'add_time' => 'Add Time',
+            'id' => '自增id',
+            'service_id' => '维修id',
+            'content' => '进度内容',
+            'process' => '进度',
+            'add_time' => '时间',
         ];
     }
 }
