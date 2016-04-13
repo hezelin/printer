e9b96c442a99935e062561829955bf264fa45003
Harry <730107711@qq.com>
2016-04-13 15:32:13 +0800

    aui css 框架配置

---
+++ assets/AuicssAsset.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * aui css 框架
+ */
+
+namespace app\assets;
+
+use yii\web\AssetBundle;
+
+class AuicssAsset extends AssetBundle
+{
+    public $basePath = '@webroot';
+    public $baseUrl = '@web';
+    public $css = [
+        'css/aui/aui.min.css',
+        'css/home_default.css',
+    ];
+    public $js = [
+//        'js/home/button_menu.js'
+    ];
+    public $depends = [
+//        'app\assets\IscrollAsset',
+//        'app\assets\ZeptoAsset',
+    ];
+}
+
+
---
+++ views/layouts/auicss.php
@@ -0,0 +1,31 @@
+<?php
+use yii\helpers\Html;
+use yii\helpers\Url;
+use app\assets\AuicssAsset;
+
+AuicssAsset::register($this);
+?>
+<?php $this->beginPage() ?>
+<!DOCTYPE html>
+<html lang="<?= Yii::$app->language ?>">
+<head>
+    <meta charset="<?= Yii::$app->charset ?>"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black">
+    <meta name="format-detection" content="telephone=no">
+    <title><?= Html::encode($this->title) ?></title>
+    <?php $this->head() //引入资源文件?>
+
+</head>
+<body>
+<?php $this->beginBody() ?>
+<?= $content ?>
+<div id="wechat-footer">
+<div class="copyright">技术支持：<?= Yii::$app->name ?></div>
+</div>
+<?php $this->endBody() ?>
+
+</body>
+</html>
+<?php $this->endPage() ?>
