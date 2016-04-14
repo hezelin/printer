98196b93dc0280acdce758d837312d3e9af674f7
Harry <730107711@qq.com>
2016-04-14 16:56:18 +0800

    字体图标替代 图片图标

--- views/wechat/maintain.php
+++ views/wechat/maintain.php
@@ -7,7 +7,7 @@ $this->title = '维修主页';
     <ul class="aui-list-view aui-grid-view">
         <li class="aui-list-view-cell aui-img aui-col-xs-6">
             <a href="<?= Url::toRoute(['m/initiative','id'=>$setting['wx_id']]) ?>" class="link-wrap">
-                <span class="aui-iconfont aui-icon-pullup aui-color"></span>
+                <span class="iconfont icon-huidanjieshou aui-color"></span>
                 <h2 class="aui-img-body aui-text-default">主动接单</h2>
                 <?php if($num['order']>0):?>
                     <span class="aui-badge aui-badge-danger"><?=$num['order']?></span>
@@ -16,7 +16,7 @@ $this->title = '维修主页';
         </li>
         <li class="aui-list-view-cell aui-img aui-col-xs-6">
             <a href="<?= Url::toRoute(['m/task','id'=>$setting['wx_id']]) ?>" class="link-wrap">
-                <span class="aui-iconfont aui-icon-repair aui-color"></span>
+                <span class="iconfont icon-weixiu aui-color"></span>
                 <h2 class="aui-img-body aui-text-default">维修任务</h2>
                 <?php if($num['fault']>0):?>
                     <span class="aui-badge aui-badge-danger"><?=$num['fault']?></span>
@@ -25,25 +25,25 @@ $this->title = '维修主页';
         </li>
         <li class="aui-list-view-cell aui-img aui-col-xs-6">
             <a href="<?= Url::toRoute(['/shop/parts/my','id'=>$setting['wx_id']]) ?>" class="link-wrap">
-                <span class="aui-iconfont aui-icon-list aui-color"></span>
+                <span class="iconfont icon-match aui-color"></span>
                 <h2 class="aui-img-body aui-text-default">配件列表</h2>
             </a>
         </li>
         <li class="aui-list-view-cell aui-img aui-col-xs-6">
             <a href="<?= Url::toRoute(['m/help','id'=>$setting['wx_id']]) ?>" class="link-wrap">
-                <span class="aui-iconfont aui-icon-form aui-color"></span>
+                <span class="iconfont icon-ziliao aui-color"></span>
                 <h2 class="aui-img-body aui-text-default">查看资料</h2>
             </a>
         </li>
         <li class="aui-list-view-cell aui-img aui-col-xs-6">
             <a href="<?= Url::toRoute(['m/index','id'=>$setting['wx_id']]) ?>" class="link-wrap">
-                <span class="aui-iconfont aui-icon-rank aui-color"></span>
+                <span class="iconfont icon-yeji110 aui-color"></span>
                 <h2 class="aui-img-body aui-text-default">我的业绩</h2>
             </a>
         </li>
         <li class="aui-list-view-cell aui-img aui-col-xs-6">
             <a href="<?= Url::toRoute(['m/notice','id'=>$setting['wx_id']]) ?>" class="link-wrap">
-                <span class="aui-iconfont aui-icon-notice aui-color"></span>
+                <span class="iconfont icon-tongzhi aui-color"></span>
                 <h2 class="aui-img-body aui-text-default">最新通知</h2>
                 <?php if($num['new']>0):?>
                     <span class="aui-badge aui-badge-danger"><?=$num['new']?></span>
