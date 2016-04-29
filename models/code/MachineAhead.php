32fab348b32d553b204fcfc5bb8363ff58e3a9c7
Harry <730107711@qq.com>
2016-04-29 15:46:50 +0800

    批量生成无资料机器二维码，增加生成租赁关系

--- models/code/MachineAhead.php
+++ models/code/MachineAhead.php
@@ -9,6 +9,7 @@ class MachineAhead
 {
     private $_wid;
     private $_num;
+    public  $seriesData = [];
     /*
      * 构建默认的数据
      */
@@ -19,7 +20,7 @@ class MachineAhead
     }
 
     /*
-     * 创建
+     * 创建机器单元
      */
     public function addItems()
     {
@@ -28,9 +29,39 @@ class MachineAhead
         $date = date('Y-m-d',$t);
 
         for($i=$this->_num; $i>0; $i--){
-            $items[] = [$this->_wid,0,$this->getSeriesId(),$date,0,4,$t];
+            $tmpSeries = $this->getSeriesId();
+            $this->seriesData[] = $tmpSeries;
+            $items[] = [$this->_wid,0,$tmpSeries,$date,0,4,$t];
         }
         return $items;
+    }
+
+    /*
+     * 创建租赁资料单元
+     */
+    public function addApply()
+    {
+        $items = [];
+        $due_time = strtotime('10 year',time());
+        foreach($this->getMachineIdBySeries() as $m){
+            $tmpSeries = $this->getSeriesId();
+            $this->seriesData[] = $tmpSeries;
+            $items[] = ['未设置','未设置',0,$this->_wid,$m['id'],$m['series_id'],$due_time,$due_time,3,2];
+        }
+        return $items;
+    }
+
+    /*
+     * 获取机器id /机器系列号
+     */
+    public function getMachineIdBySeries()
+    {
+        $data = (new \yii\db\Query())
+            ->select('id,series_id')
+            ->from('tbl_machine')
+            ->where(['wx_id'=>$this->_wid,'series_id'=>$this->seriesData])
+            ->all();
+        return $data;
     }
 
     /*
@@ -38,10 +69,13 @@ class MachineAhead
      */
     public function createAll()
     {
-        return Yii::$app->db->createCommand()->batchInsert('tbl_machine',
+        $row = Yii::$app->db->createCommand()->batchInsert('tbl_machine',
             ['wx_id', 'model_id','series_id','buy_date','buy_price','come_from','add_time'],$this->addItems())->execute();
 
-
+        if($row){
+            Yii::$app->db->createCommand()->batchInsert('tbl_rent_apply',
+                ['phone', 'name', 'project_id','wx_id','machine_id', 'openid','due_time','first_rent_time','rent_period','status'],$this->addApply())->execute();
+        }
     }
 
     /*
