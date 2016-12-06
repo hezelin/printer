018b40c8e075fd5d529435adc3c366c73ddc4f79
biaofei <biaofei>
2016-12-06 14:02:55 +0800

    20161206 修改.gitignore

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=printer',
-    'username' => 'root',
-    'password' => 'oursla321',
-    'charset' => 'utf8mb4',
-];
