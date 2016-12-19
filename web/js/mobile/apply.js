c4261cf991d1aeeaeb4436be2e20a74166aa103e
biaofei <biaofei>
2016-12-19 18:25:01 +0800

    20161219 修改维修申请的Bug

--- web/js/mobile/apply.js
+++ web/js/mobile/apply.js
@@ -163,16 +163,27 @@ $(function(){
     $("#wechat-submit").click(function(){
         var i = 0, length = images.localId.length;
 
-        if (images.localId.length == 0){
-            alert('图片不能为空！');
-            return false;
-        }
+        //[20161219 modify
+        //if (images.localId.length == 0){
+        //    alert('图片不能为空！');
+        //    return false;
+        //}
+        var flag = 0;
+        //alert(length);
+        if($('#service-desc').val() != '' || images.localId.length != 0 || voice_localId != undefined)//判断文字、图片和语音是否为空
+            flag = 1;
+
+
+        if(flag != 1)
+            alert('文字、图片和语音至少选择一种提交！');
+        //20161219]
 
         if( $('#voice-wrap').attr('data-value') == '2'){
             alert('正在录音中....');
             return false;
         }
-
+        alert('可以提交');
+        return;
         function upload() {
             wx.uploadImage({
                 localId: images.localId[i],
